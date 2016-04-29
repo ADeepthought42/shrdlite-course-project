1e9d960a6968fabc785e5cdf3b3f396eb5b5b27f
Daniel Andersson <andersson.d.92@gmail.com>
2016-04-29 21:25:26 +0200

    A* search algorithm - first iteration
    No timeout implemented, not tested

--- Graph.ts
+++ Graph.ts
@@ -33,6 +33,8 @@ class SearchResult<Node> {
     cost : number;
 }
 
+
+
 /**
 * A\* search implementation, parameterised by a `Node` type. The code
 * here is just a template; you should rewrite this function
@@ -46,7 +48,8 @@ class SearchResult<Node> {
 * @param goal A function that returns true when given a goal node. Used to determine if the algorithm has reached the goal.
 * @param heuristics The heuristic function. Used to estimate the cost of reaching the goal from a given Node.
 * @param timeout Maximum time (in seconds) to spend performing A\* search.
-* @returns A search result, which contains the path from `start` to a node satisfying `goal` and the cost of this path.
+* @returns A search result, which contains the path from `start` to a node 
+* satisfying `goal` and the cost of this path.
 */
 function aStarSearch<Node> (
     graph : Graph<Node>,
@@ -55,19 +58,81 @@ function aStarSearch<Node> (
     heuristics : (n:Node) => number,
     timeout : number
 ) : SearchResult<Node> {
-    // A dummy search result: it just picks the first possible neighbour
+        // A dummy search result: it just picks the first possible neighbour
     var result : SearchResult<Node> = {
         path: [start],
         cost: 0
     };
-    while (result.path.length < 3) {
-        var edge : Edge<Node> = graph.outgoingEdges(start) [0];
-        if (! edge) break;
-        start = edge.to;
-        result.path.push(start);
-        result.cost += edge.cost;
+
+    var rs : collections.LinkedDictionary<Node,SearchResult<Node>> = 
+        new collections.LinkedDictionary<Node,SearchResult<Node>>();
+    var openNodes : collections.PriorityQueue<Node> = 
+        new collections.PriorityQueue<Node>(compare);
+    var closedNodes : collections.LinkedList<Node> = 
+        new collections.LinkedList<Node>();
+
+    
+    var compare = (a : Node, b : Node) : number =>
+    {
+        //a is lesser than b by some ordering criterion
+        if(rs.getValue(a).cost > rs.getValue(b).cost) {
+            return -1;
+        }
+        //a is greater than b by the ordering criterion
+        if(rs.getValue(a).cost < rs.getValue(b).cost) {
+            return 1;
+        } 
+          // a must be equal to b
+        return 0;
     }
-    return result;
+
+    var startCost : number = heuristics(start);
+    var currentNode : Node = start;
+    result.cost = startCost;
+    rs.setValue(currentNode,result);
+    openNodes.add(currentNode);
+    
+
+    while (!openNodes.isEmpty()) {
+
+        currentNode = openNodes.dequeue();
+        closedNodes.add(currentNode);
+        result = rs.getValue(currentNode);
+
+        if(goal(currentNode))
+            return result;
+
+        var edges = graph.outgoingEdges(currentNode);
+
+        for(var i ; i < edges.length ; i++){
+            
+            var neighbour = edges[i];
+            var res : SearchResult<Node> = result;
+            var cost = res.cost + neighbour.cost;
+
+            var neighbourRes = rs.getValue(neighbour.to);
+            var neighbourCost = neighbourRes.cost;
+            if(openNodes.contains(neighbour.to)) {
+                if(cost < neighbourCost){
+                    //Ta bort neighbour från openNodes. Hur??
+                }
+                
+            }
+            if(closedNodes.contains(neighbour.to)) {
+                if(cost < neighbourCost){
+                    closedNodes.remove(neighbour.to);
+                }
+            }
+            else {
+                res.cost = cost + heuristics(neighbour.to);
+                res.path.push(neighbour.to);
+                rs.setValue(neighbour.to, res);
+                openNodes.add(neighbour.to);
+            }
+        }
+    }
+
+
 }
 
 
